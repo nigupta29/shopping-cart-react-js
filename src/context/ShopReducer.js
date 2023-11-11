96d045b9edab330b1247c7dfde5c0ac81311d7c9
Nikhil Gupta <nikhil.gupta5667@gmai.com>
2023-11-11 11:14:59 +0530

    added remove from cart action

--- src/context/ShopReducer.js
+++ src/context/ShopReducer.js
@@ -8,9 +8,13 @@ const productReducer = (state, action) => {
           : product
       )
 
-    case 'REMOVE_FROM_CART': {
-      return state
-    }
+    case 'REMOVE_FROM_CART':
+      // Increase the stock of the product when removed from the cart
+      return state.map((product) =>
+        product.id === action.payload.id
+          ? { ...product, stock: product.stock + 1 }
+          : product
+      )
 
     default:
       throw new Error('Unknown Dispatch function for Product Reducer')
@@ -21,25 +25,35 @@ const cartReducer = (state, action) => {
   switch (action.type) {
     case 'ADD_TO_CART': {
       const existingItem = state.find((item) => item.id === action.payload.id)
-      const { id, title, img_src, price, stock } = action.payload
 
       // Check if the product is already in the cart
       if (existingItem) {
         return state.map((item) =>
           item.id === action.payload.id
-            ? { ...item, quantity: item.quantity + 1, stock: stock - 1 }
+            ? { ...item, quantity: item.quantity + 1, stock: item.stock - 1 }
             : item
         )
       }
 
       // If the product is not in the cart, add it with quantity 1
+      const { id, title, img_src, price, stock } = action.payload
       return [
         ...state,
         { id, title, img_src, price, stock: stock - 1, quantity: 1 }
       ]
     }
     case 'REMOVE_FROM_CART': {
-      return state
+      const existingItem = state.find((item) => item.id === action.payload.id)
+
+      if (existingItem.quantity === 1) {
+        return state.filter((item) => item.id !== existingItem.id)
+      }
+
+      return state.map((item) =>
+        item.id === action.payload.id
+          ? { ...item, quantity: item.quantity - 1, stock: item.stock + 1 }
+          : item
+      )
     }
 
     default:
